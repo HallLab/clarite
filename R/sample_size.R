31f0d37820a79e5202556bd098496b8edc28aaac
Anastasia Lucas <amlucas@client-75-102-127-166.mobility.psu.edu>
2018-02-14 13:19:54 -0500

    Added function

---
+++ R/sample_size.R
@@ -0,0 +1,17 @@
+#' sample_size
+#'
+#' Create list of sample_sizes for each variable
+#' @param df data frame with IID as first column
+#' @return data frame with two columns, "Variable" and "Sample_Size"
+#' @export
+#' @examples
+#' sample_size(df)
+
+sample_size <- function(df) {
+
+  df_n <- as.data.frame(sapply(df, function(x) (length(na.omit(x)))))
+  df_n <- cbind(rownames(df_n), data.frame(df_n, row.names=NULL))
+  names(df_n) <- c("Variable", "Levels")
+
+  return(df_n)
+}
