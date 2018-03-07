35e26f41b4c28809764ff2c2a9dc7cea1d00cdca
Anastasia Lucas <anastasia.lucas@pennmedicine.upenn.edu>
2018-03-07 13:17:12 -0500

    Updated to allow list or dataframe input

--- R/samplefilter.R
+++ R/samplefilter.R
@@ -2,7 +2,7 @@
 #'
 #' Subset data by sample name
 #' @param d data frame, with first column as IID
-#' @param samples list of samples to subset
+#' @param samples list or dataframe of samples to subset
 #' @param exclude boolean, keep or exclude
 #' @return dataframe
 #' @export
@@ -14,10 +14,19 @@ samplefilter <- function(d, samples, exclude=FALSE){
   if(is.element('IID', names(d))==FALSE){
     stop("Please add IID to d as column 1")
   }
+
   if(exclude==FALSE){
-    subd <- d[d$IID %in% samples, , drop=FALSE]
+    if(is.data.frame(samples)==TRUE){
+      subd <- d[d$IID %in% samples[,1], , drop=FALSE]
+    } else {
+      subd <- d[d$IID %in% samples, , drop=FALSE]
+    }
   } else {
-    subd <- d[!d$IID %in% samples, , drop=FALSE]
+    if(is.data.frame(samples)==TRUE){
+      subd <- d[!d$IID %in% samples[,1], , drop=FALSE]
+    } else {
+      subd <- d[!d$IID %in% samples, , drop=FALSE]
+    }
   }
 
   return(subd)
