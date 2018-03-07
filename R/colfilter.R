a097a9b7f18126e659ee9d594f319a2d9a01c1a1
Anastasia Lucas <anastasia.lucas@pennmedicine.upenn.edu>
2018-03-07 13:05:57 -0500

    Updated to allow list or dataframe input

--- R/colfilter.R
+++ R/colfilter.R
@@ -2,7 +2,7 @@
 #'
 #' Subset data by column name
 #' @param d data frame
-#' @param cols list of variable names to subset
+#' @param cols list or dataframe of variable names to subset
 #' @param exclude boolean, keep or exclude
 #' @return dataframe
 #' @export
@@ -12,9 +12,17 @@
 
 colfilter <- function(d, cols, exclude=FALSE){
   if(exclude==FALSE){
-    subd <- d[, colnames(d) %in% cols, drop=FALSE]
+    if(is.data.frame(cols)==TRUE){
+      subd <- d[, colnames(d) %in% cols[,1], drop=FALSE]
+    } else {
+      subd <- d[, colnames(d) %in% cols, drop=FALSE]
+    }
   } else {
-    subd <- d[, !colnames(d) %in% cols, drop=FALSE]
+    if(is.data.frame(cols)==TRUE){
+      subd <- d[, !colnames(d) %in% cols[,1], drop=FALSE]
+    } else {
+      subd <- d[, !colnames(d) %in% cols, drop=FALSE]
+    }
   }
 
   return(subd)
