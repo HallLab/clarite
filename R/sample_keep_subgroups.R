6998d38de6e881ff57462cf1f31fea7687cba84e
Anastasia Lucas <anastasia.lucas@pennmedicine.upenn.edu>
2018-03-08 12:40:02 -0500

    Removed IID requirement

--- R/sample_keep_subgroups.R
+++ R/sample_keep_subgroups.R
@@ -1,7 +1,6 @@
-#' sample_keep_subgroups
 #'
 #' Keep variables with a minimum of n samples
-#' @param df data frame with IID as first column
+#' @param df data frame
 #' @param n minimum sample size desired
 #' @return data frame containing only those variables with at least n samples per factor level
 #' @export
@@ -10,14 +9,12 @@
 
 
 sample_keep_subgroups <- function(df, n=200){
-
-  if(is.element('IID', names(df))==FALSE){
-    stop("Please add IID to dataframe as column 1")
+  
+  keep <- df[, sapply(df, function(v) (min(table(v)) > n)), drop=FALSE]
+  
+  if(is.element('IID', names(df))==TRUE){
+    keep <- cbind("IID"=df$IID, keep)
   }
-
-  keep <- df[, sapply(df, function(v) (min(table(v)) > n))]
-  keep <- cbind("IID"=df$IID, keep)
-
+  
   return(keep)
 }
-
