525c51a8d6a27b97dddeb9776188604cfd8e13a6
Anastasia <anastasia.lucas@pennmedicine.upenn.edu>
2019-02-08 16:34:10 -0500

    Declare global variables

---
+++ R/globalVariables.R
@@ -0,0 +1,7 @@
+#' \code{clarite} package
+#'
+#' @docType package
+#' @name clarite
+
+utils::globalVariables(c("Color", "Shape", "Variable", "pos_index",
+                         "posmax", "posmin", "pvalue", "shademap"))
